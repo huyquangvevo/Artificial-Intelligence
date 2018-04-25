ca85fc74903f8e8328212ae4734e45bafcce41d5
QuangHuy <quanghuy27101997@gmail.com>
2018-04-25 18:09:54 +0700

    First Commit

---
+++ app/src/main/java/com/example/dell/caro2/Score.java
@@ -0,0 +1,15 @@
+package com.example.dell.caro2;
+
+/**
+ * Created by Dell on 4/15/2018.
+ */
+
+public class Score {
+    int scoreDirect;
+    Cell cellStart;
+    Cell cellEnd;
+
+    public Score(int scoreDirect) {
+        this.scoreDirect = scoreDirect;
+    }
+}
